84f0b6c54f27a7292f7d7899fe8d84d1c344194e
sarvesh10491 <sarvesh10491@gmail.com>
2018-06-05 13:30:30 -0700

    Add files via upload

---
+++ appCode/app/src/main/java/com/eeg/group31/properties/Constants.java
@@ -0,0 +1,15 @@
+package com.eeg.group31.properties;
+
+public class Constants {
+    public final static int NUM_FEATURES = 720;
+    public final static int NUM_CLASSES = 32;
+    public final static int NUM_TRAIN_DATA = 1000; //1120 = 70*16
+    public final static int NUM_TEST_DATA = 120;
+    public final static String CLOUD_URL = "http://54.183.231.12:3000/server/";
+    public final static String FOG_URL = "http://192.168.43.43:3000/server/";
+    public final static String MODEL_FILE = "modelFile.txt";
+    public final static String TRAIN_DATA = "train_Data.txt";
+    public final static String TEST_DATA = "testData/";
+    public final static String PREDICT_DATA =  "predicted.txt";
+    public final static String APP_FOLDER = "Android/data/CSE535_PROJECT/";
+}
